d770a4d2a1e79b5385f66d96e0d9a8db44908360
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2024-06-19 16:17:54 +0300

    add home router and remove debug banner

--- lib/core/router/router.dart
+++ lib/core/router/router.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:slash_store/core/router/router_helper.dart';
+import 'package:slash_store/features/home/presentation/home_screen/home.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../../features/home/presentation/home_screen/cubit/home_cubit.dart';
 
-abstract class AppRouter{
+abstract class AppRouter {
   static final Map<String, WidgetBuilder> generateRoutes = {
-
-
+    RouterHelper.home: (context) =>
+        BlocProvider(
+          create: (context) => HomeCubit(),
+          child: const HomeScreen(),
+        ),
   };
 }
