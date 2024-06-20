e30ac8e4af8f12e853a2a9c5a5a78e3355cf66dd
Mohamed Gammal <73348912+mhammadgammal@users.noreply.github.com>
2024-06-20 21:28:57 +0300

    rearrange app_images/dart

---
+++ lib/core/theme/app_images.dart
@@ -0,0 +1,29 @@
+abstract class AppImages {
+
+  static const bestSeller1 = 'assets/images/best_seller_1.png';
+  static const bestSeller2 = 'assets/images/best_seller_2.png';
+  static const bestSeller3 = 'assets/images/best_seller_3.png';
+
+  static const newArrival1 = 'assets/images/new_arrival_1.png';
+  static const newArrival2 = 'assets/images/new_arrival_1.png';
+  static const newArrival3 = 'assets/images/new_arrival_1.png';
+
+  static const recommended1 = 'assets/images/recommended_1.png';
+  static const recommended2 = 'assets/images/recommended_2.png';
+  static const recommended3 = 'assets/images/recommended_3.png';
+
+  static const blackFridayOffer = 'assets/images/black_friday_offer.png';
+  static const bestDealOffer = 'assets/images/best_deal.png';
+
+  static const locationIc = 'assets/images/Vector.png';
+  static const notificationIc = 'assets/images/reminder.png';
+
+  //categories
+  static const fashionIc = 'assets/images/categories/shirt.png';
+  static const gamesIc = 'assets/images/categories/dice.png';
+  static const accessoriesIc = 'assets/images/categories/glasses.png';
+  static const booksIc = 'assets/images/categories/book.png';
+  static const artifactsIc = 'assets/images/categories/artifacts.png';
+  static const petsCareIc = 'assets/images/categories/pet.png';
+
+}
